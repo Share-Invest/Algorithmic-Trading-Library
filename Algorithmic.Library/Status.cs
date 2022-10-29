a111e8fd57827e3441a90a8ef7569c85f9973fbf
SANGWOO PARK <cyberprophet@hanmail.net>
2022-10-30 00:52:07 +0900

    초기 커밋.

---
+++ Algorithmic.Library/Status.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+namespace ShareInvest
+{
+    public static class Status
+    {
+        public static string Address => IsDebugging ? "" : url;
+
+        public static bool IsDebugging
+        {
+            get; private set;
+        }
+        public static void SetDebug()
+        {
+            IsDebugging = true;
+
+            Debug.WriteLine(nameof(SetDebug));
+        }
+        const string url = "https://coreapi.shareinvest.net";
+    }
+}
