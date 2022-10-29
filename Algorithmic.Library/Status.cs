bfb210a2cfe0b20470a412aa4d5b71c79610f1b6
SANGWOO PARK <cyberprophet@hanmail.net>
2022-10-30 01:05:51 +0900

    initialize for a new branch

--- Algorithmic.Library/Status.cs
+++ Algorithmic.Library/Status.cs
@@ -1,4 +1,6 @@
 ﻿using System.Diagnostics;
+using System.Runtime.Versioning;
+using System.Security.Principal;
 
 namespace ShareInvest
 {
@@ -6,10 +8,22 @@ namespace ShareInvest
     {
         public static string Address => IsDebugging ? "" : url;
 
+        [SupportedOSPlatform("windows8.0")]
+        public static bool IsAdministrator
+        {
+            get
+            {
+                using (var cur = WindowsIdentity.GetCurrent())
+                {
+                    return new WindowsPrincipal(cur).IsInRole(WindowsBuiltInRole.Administrator);
+                }
+            }
+        }
         public static bool IsDebugging
         {
             get; private set;
         }
+        [Conditional("DEBUG")]
         public static void SetDebug()
         {
             IsDebugging = true;
