4bddfdd1e547741d6593eac2dfb894917a451ba2
SANGWOO PARK <cyberprophet@hanmail.net>
2022-11-11 00:58:13 +0900

    gonna have to flow smoothly

---
+++ Algorithmic.Library/Observers/OpenAPI/RealMessageEventArgs.cs
@@ -0,0 +1,23 @@
+﻿namespace ShareInvest.Observers.OpenAPI;
+
+public class RealMessageEventArgs : MessageEventArgs
+{
+    public string Type
+    {
+        get;
+    }
+    public string Key
+    {
+        get;
+    }
+    public string Data
+    {
+        get;
+    }
+    public RealMessageEventArgs(string type, string key, string data)
+    {
+        Type = type;
+        Key = key;
+        Data = data;
+    }
+}
