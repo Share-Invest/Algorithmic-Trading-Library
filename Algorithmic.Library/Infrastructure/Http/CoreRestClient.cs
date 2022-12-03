a406a26179ba04117a55389c85ee19582aeea21b
SANGWOO PARK <cyberprophet@hanmail.net>
2022-12-03 20:37:54 +0900

    implement recusize logic publish & update

--- Algorithmic.Library/Infrastructure/Http/CoreRestClient.cs
+++ Algorithmic.Library/Infrastructure/Http/CoreRestClient.cs
@@ -1,9 +1,12 @@
-﻿using RestSharp;
+﻿using Newtonsoft.Json.Linq;
+
+using RestSharp;
 
 using ShareInvest.Properties;
 
 using System.Diagnostics;
 using System.Net;
+using System.Text;
 
 namespace ShareInvest.Infrastructure.Http;
 
@@ -13,11 +16,34 @@ public class CoreRestClient : RestClient, ICoreClient
     {
         var transformer = Identifies.Parameter.TransformOutbound(route);
 
-        var request = new RestRequest($"{Resources.KIWOOM}/{transformer}", Method.POST);
+        var request = new RestRequest($"{Resources.KIWOOM}/{transformer}",
+                                      Method.POST);
 
         request.AddJsonBody(param);
 
-        var res = await ExecuteAsync(request, cancellationTokenSource.Token);
+        var res = await ExecuteAsync(request,
+                                     cancellationTokenSource.Token);
+
+        if (HttpStatusCode.OK != res.StatusCode)
+        {
+#if DEBUG
+            Debug.WriteLine(res.StatusCode);
+#endif
+        }
+        return res.Content;
+    }
+    public async Task<object> GetAsync(string route, JToken token)
+    {
+        var resource = string.Concat(Resources.KIWOOM,
+                                     '/',
+                                     Identifies.Parameter.TransformQuery(token,
+                                                                         new StringBuilder(route)));
+#if DEBUG
+        Debug.WriteLine(resource);
+#endif
+        var res = await ExecuteAsync(new RestRequest(resource,
+                                                     Method.GET),
+                                     cancellationTokenSource.Token);
 
         if (HttpStatusCode.OK != res.StatusCode)
         {
